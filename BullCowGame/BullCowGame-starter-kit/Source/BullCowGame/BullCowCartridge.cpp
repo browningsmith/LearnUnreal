000b1d244dcd394d4ee71aaafd87ed3ff78f7a44
Browning Smith <browningsmith@email.arizona.edu>
2021-01-11 20:05:58 -0700

    Added entry point for custom code

--- BullCowGame/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,11 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    PrintLine("Hello There!");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
