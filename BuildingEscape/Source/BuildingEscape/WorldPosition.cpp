4db63994990139f110e9abda44771d897f9ab888
Browning Smith <browningsmith@email.arizona.edu>
2021-01-14 00:09:39 -0700

    Added some resources for a door model from the class website

--- BuildingEscape/Source/BuildingEscape/WorldPosition.cpp
+++ BuildingEscape/Source/BuildingEscape/WorldPosition.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -19,9 +20,9 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Display, TEXT("Hello in Gray!"));
-	UE_LOG(LogTemp, Warning, TEXT("Hello in Yellow!"));
-	UE_LOG(LogTemp, Error, TEXT("Hello in Red!"));
+	FString ActorName = GetOwner()->GetName();
+	FString Location = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is located at %s"), *ActorName, *Location);
 }
 
 
