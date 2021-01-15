2402d13d4634ee06e1baab2a736291d00c8e542d
Browning Smith <browningsmith@email.arizona.edu>
2021-01-15 00:21:25 -0700

    Door opens on game start

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,34 @@
+// Copyright Browning Keith Smith 2021
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	float NewYaw = FMath::Lerp(GetOwner()->GetActorRotation().Yaw, TargetYaw, 0.05f);
+	FRotator NewRotation = {0.f, NewYaw, 0.f};
+	GetOwner()->SetActorRotation(NewRotation);
+}
+
